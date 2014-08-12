87a4ec9b3cf248c141e2d068fdb3fd828ca82986
Max A <Max_A@here.com>
2014-08-12 11:22:56 -0700

    Down arrow to stop paddle

--- bounce.py
+++ bounce.py
@@ -46,20 +46,24 @@ class Paddle:
         self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        self.canvas.bind_all('<KeyPress-Down>', self.stop)
 
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
         pos = self.canvas.coords(self.id)
         if pos[0] <= 0:
-            self.x = 0
+            self.x = -self.x
         elif pos[2] >= self.canvas_width:
-            self.x = 0
+            self.x = -self.x
 
     def turn_left(self, evt):
         self.x = -2
 
     def turn_right(self, evt):
         self.x = 2
+
+    def stop(self, evt):
+        self.x = 0
 
 tk = Tk()
 tk.title('Bounce')
@@ -79,3 +83,5 @@ while 1:
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+    
+
