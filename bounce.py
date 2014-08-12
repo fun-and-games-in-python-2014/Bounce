1ad4b00cdff7ffa079b5c5845b68ed70bd934429
Max A <Max_A@here.com>
2014-08-12 11:27:31 -0700

    Very basic sketch of menu key

--- bounce.py
+++ bounce.py
@@ -73,6 +73,11 @@ canvas=Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
+def menu(evt):
+    pass
+
+canvas.bind_all('<KeyPress-x>', menu)
+
 paddle = Paddle(canvas, 'blue')
 ball = Ball(canvas, paddle, 'red')
 
